267f52806c8b2499ebc058a5f3965d84fcc19b08
Nicolas Brett <nicolasb59@gmail.com>
2019-06-14 11:48:25 -0400

    Made dino objects and console logged their values

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,28 +7,49 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-
+let dino1 = {
+  "name": "tyrannosaurus",
+  "diet": "carnivorous",
+  weight: "7000kg",
+  "length": "9m",
+  "period": "Late Cretaceous"
+}
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+let dino2 = {
+  "name": "stegosaurus",
+  "diet": "herbivorous",
+  "weight": "2000kg",
+  "length": "9m",
+  "period": "Late Jurassic"
+}
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
+let dino3 = {
+  "name": "velociraptor",
+  "diet": "carnivorous",
+  "weight": "15kg",
+  "length": "9m",
+  "period": "Late Cretaceous"
+}
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log("tyrannosaurus weighs: " + dino1.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(`diet of a velociraptor is:  ${dino2.diet}`);
 
 // How long was a stegosaurus?
-console.log();
+console.log(`a stegosaurus length is: ${dino3.length}`);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(`time period of a tyrannosaurs: ${dino1.period}`);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+dino1.roar = function(){
+  return "RAWERSRARARWERSARARARRRR";
+}
+console.log(`Listen to this! ${dino1.roar()}`);
 
 
 // ==== Arrays ====
